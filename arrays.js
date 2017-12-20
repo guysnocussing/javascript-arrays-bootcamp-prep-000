f9a6269ca1c9df057c0ec2a2a6c7d5c8d4d25f77
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-20 00:15:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(arr, el) {
   var newArr = arr.unshift(el);
