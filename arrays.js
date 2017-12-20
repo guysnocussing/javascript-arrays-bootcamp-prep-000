3e3849a510317a65632c8fbceb34d7689b297599
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-20 00:10:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+function addElementToBeginningOfArray(arr, el) {
+  var newArr = arr.unshift(el);
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, el) {
+  return arr.unshift(el); 
+}
